d27c8e1b0d4d468541c7c661e78656968ed2bd98
Sxlly <shaesullivan.buisness1@gmail.com>
2021-10-13 15:16:57 +0800

    formatting changes

--- src/getDrugInteraction.js
+++ src/getDrugInteraction.js
@@ -119,69 +119,68 @@ export default () => {
     return (
 
         <div>
-            <header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
-                    <div className="container">
-                        <a href="#" className="navbar-brand">Drug<span className="text-primary">Interactor</span></a>
-
-                        <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-
-                        <div className="navbar-collapse collapse" id="navbarContent">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="">All Drugs</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
+            
+            <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
                 <div className="container">
-                    <div className="page-banner home-banner">
-                        <div className="row align-items-center flex-wrap-reverse h-100">
-                            <div className="col-md-6 py-5 wow fadeInleft">
-                                <h1 className="mb-4">Welcome to Drug Interactor!</h1>
-                                <p className="text-lg text-grey mb-5">Take Substances Safely</p>
-                                <a href="#" className="btn btn-primary btn-split">Dive In</a>
-                            </div>
-                            <div className="col-md-6 py-5 wow zoomIn">
-                                <div className="img-fluid text-center">
-                                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/LSD_Structure.svg" alt="STRUCTURE" />
-                                </div>
-                            </div>
-                        </div>
-                        <a href="" className="btn-scroll" data-role="smoothscroll"><span className="mai-arrow-down"></span> </a>
+                    <a href="#" className="navbar-brand">Drug<span className="text-primary">Interactor</span></a>
+
+                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+
+                    <div className="navbar-collapse collapse" id="navbarContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="">All Drugs</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </header>
+            </nav>
 
             <div className="page-section">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="card-service wow fadeInUp">
-                                <div className="header">
-                                    <img src="" alt="image" />
-                                </div>
-                                <div className="body">
-                                    <h5 className="text-secondary">Drug Interactor</h5>
-                                    <p>The online tool</p>
-                                    <a href="" className="btn btn-primary">Explore</a>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="card-service-large wow fadeInUp">
+                        <form  onSubmit={onSubmit}>
+                            <h1 className="rxcui-header">Drug Interactions</h1>
+                            <h2 className="rxcui-subheader">subheader</h2>
+
+                            <input
+                                className="rxcui-name-input"
+                                id="nameOne"
+                                name="name"
+                                type="text"
+                                placeholder="Enter Drug Name..."
+                                value={nameOne}
+                                onChange={(event) => nameChangeOne(event.target.value)}
+                            />
+
+                            <button type="submit" className="rxcui-find-btn">Find</button>
+
+                            <p className="rxcui-answer">ID: {rxcuiID}</p>
+                            <p className="">{nameOne} Interacts With The Following</p>
+                            <p className="">Total Number of Drugs {nameOne} Interacts With: {interactionCount}</p>
+
+                            <ul className="interactions_list">
+                                {
+                                    interactionsList.map(Interaction => <li className="interaction" key={Interaction}>{Interaction}</li>)
+                                }
+                            </ul>
+
+                        </form>
                     </div>
                 </div>
             </div>
 
 
-        <div className="back-to-top"></div>
+
+
+
+
         <div className="main_div">
 
             <form className="form_main" onSubmit={onSubmit}>
