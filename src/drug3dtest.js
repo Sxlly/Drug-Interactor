8295f1b0038c0339197af3db4ac60df76851962d
Sxlly <shaesullivan.buisness1@gmail.com>
2021-10-25 12:29:28 +0800

    addding navbar to 3d structure page

--- src/drug3dtest.js
+++ src/drug3dtest.js
@@ -38,6 +38,41 @@ export default () => {
         
         <div>
 
+            
+                <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+                    <div className="container">
+                        <a href="/HomePage" className="navbar-brand">Drug<span className="text-primary">Interactor</span></a>
+
+                        <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+
+                        <div className="navbar-collapse collapse" id="navbarContent">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/HomePage">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/getInteractionPair">Interaction Pair Tool</a>
+                                </li>
+                                <li className="nav-link">
+                                    <a className="nav-link" href="/getDrugInteraction">All Interactions Tool</a>
+                                </li>
+                                <li className="nav-link">
+                                    <a className="nav-link" href="/AllDrugTerms">All Drugs Tool</a>
+                                </li>
+                                <li className="nav-link">
+                                    <a className="nav-link" href="/getRxcuiId">Rxcui ID Tool</a>
+                                </li>
+                                <li className="nav-link">
+                                    <a className="nav-link" href="/drug3dtest">3D Structure Tool</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
             <button onClick={onSubmit}>Test</button>
 
             <div >
