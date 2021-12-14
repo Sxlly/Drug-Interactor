e558360267ba664a892699854c41ebd18a38db5d
Sxlly <shaesullivan.buisness1@gmail.com>
2021-12-14 19:07:01 +0800

    sidebar drawer styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,8 @@ import { Link as RouterLink, useLocation } from 'react-router-dom';
 
 import { styled } from '@mui/material/styles';
 import { Box, Link, Button, Drawer, Typography, Avatar, Stack } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { StyledEngineProvider } from '@mui/material';
 
 import Scrollbar from './DrawerScrollBar';
 import DrawerSection from './drawerNavSection';
@@ -13,8 +15,39 @@ import { MHidden } from './@material-extend';
 import drawerConfig from './drawerConfig';
 import account from './demoAccount';
 
+//google font imports
+import "@fontsource/advent-pro/600.css";
+import "@fontsource/public-sans/600.css";
+
 
 const drawerWidth = 240;
+
+//material ui CSS classes
+const useStyles = makeStyles({
+
+    accountText: {
+
+        fontFamily: "Public Sans",
+        textShadow: "5px 5px 5px rgba(85,85,85,0.2)",
+        color: "#555555",
+        "&:hover,&:focus": {
+
+            transition: "ease-in-out 0.25s",
+            textShadow: "5px 5px 5px rgba(85,85,85,0.5)",
+            fontSize: "17.5px",
+        },
+    },
+
+    pageLink: {
+
+        textDecoration: "none",
+        "&:focus,&:hover": {
+
+            textDecoration: "none",
+        },
+
+    },
+});
 
 const RootStyle = styled('div')(({ theme }) => ({
     [theme.breakpoints.up('lg')]: {
@@ -26,9 +59,10 @@ const RootStyle = styled('div')(({ theme }) => ({
   const AccountStyle = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    padding: "2px 2.5px",
+    padding: "20px 25px",
     borderRadius: "30px",
-    backgroundColor: "rgba(85,85,85,0.25)",
+    backgroundColor: "rgba(85,85,85,0.2)",
+    textDecoration: "none",
   }));
 
 DashboardSidebar.propTypes = {
@@ -38,6 +72,7 @@ DashboardSidebar.propTypes = {
 
 export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
     const { pathname } = useLocation();
+    const classes = useStyles();
   
     useEffect(() => {
       if (isOpenSidebar) {
@@ -47,40 +82,43 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
     }, [pathname]);
   
     const renderContent = (
-      <Scrollbar
-        sx={{
-          height: '100%',
-          '& .simplebar-content': { height: '100%', display: 'flex', flexDirection: 'column' }
-        }}
-      >
-        <Box sx={{ px: 2.5, py: 3 }}>
-          <Box component={RouterLink} to="/" sx={{ display: 'inline-flex' }}>
-            <h1>DI</h1>
-          </Box>
-        </Box>
-  
-        <Box sx={{ mb: 5, mx: 2.5 }}>
-          <Link underline="none" component={RouterLink} to="#">
-            <AccountStyle>
-              <Avatar src={account.photoURL} alt="photoURL" />
-              <Box sx={{ ml: 2 }}>
-                <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                  {account.displayName}
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                  {account.role}
-                </Typography>
-              </Box>
-            </AccountStyle>
-          </Link>
-        </Box>
-  
-        <DrawerSection navConfig={drawerConfig} />
-  
-        <Box sx={{ flexGrow: 1 }} />
-  
+        
+        <StyledEngineProvider injectFirst>
+            <Scrollbar
+                sx={{
+                height: '100%',
+                '& .simplebar-content': { height: '100%', display: 'flex', flexDirection: 'column' }
+                }}
+            >
+                <Box sx={{ px: 2.5, py: 3 }}>
+                <Box component={RouterLink} to="/" sx={{ display: 'inline-flex' }}>
+                    <h1>DI</h1>
+                </Box>
+                </Box>
+        
+                <Box sx={{ mb: 5, mx: 2.5 }}>
+                <Link className={classes.pageLink} component={RouterLink} to="#">
+                    <AccountStyle>
+                    <Avatar src={account.photoURL} alt="photoURL" />
+                    <Box sx={{ ml: 2 }}>
+                        <Typography variant="subtitle2" className={classes.accountText} >
+                        {account.displayName}
+                        </Typography>
+                        <Typography variant="body2" className={classes.accountText}>
+                        {account.role}
+                        </Typography>
+                    </Box>
+                    </AccountStyle>
+                </Link>
+                </Box>
+        
+                <DrawerSection navConfig={drawerConfig} />
+        
+                <Box sx={{ flexGrow: 1 }} />
+        
 
-      </Scrollbar>
+            </Scrollbar>
+        </StyledEngineProvider>
     );
   
     return (
--- src/components/drawerConfig.js
+++ src/components/drawerConfig.js
@@ -19,7 +19,7 @@ const drawerConfig = [
     {
       title: 'Interaction Pair Tool',
       path: '/dashboard/products',
-      icon: <svg width="30px" height="30px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" /></svg>
+      icon: <svg width="30px" height="30px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path></svg>
     },
     {
       title: 'All Interactions Tool',
