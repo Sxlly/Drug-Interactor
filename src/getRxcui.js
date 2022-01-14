fb0e15913985183a326d345c8eb6e071b75987ba
Sxlly <shaesullivan.buisness1@gmail.com>
2022-01-14 23:36:10 +0800

    linking rxcui page

--- src/getRxcui.js
+++ src/getRxcui.js
@@ -1,10 +1,320 @@
 import React, { Component, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import './getRxcuiCSS.css';
+import './getDrugInteractionCSS.css';
+import './bootstrap.css';
+import './theme.css';
 import * as ReactBootStrap from "react-bootstrap";
 
+//importing self made components
+import AppBarSearch from './components/appBarSearch';
+import AccountPopover from './components/AccountPopOver';
+import DashboardSidebar from './components/Sidebar';
 
-export default () => {
+//Material UI Icon Imports
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
+//Material UI Imports
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { TextField } from "@mui/material";
+import { Stack, StyledEngineProvider } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Card, CardActions, CardContent, CardMedia, Grid } from '@mui/material';
+
+
+//google font imports
+import "@fontsource/advent-pro/600.css";
+import "@fontsource/public-sans/600.css";
+
+//sidebar width constant
+const drawerWidth = 240;
+
+
+//material ui CSS classes
+const useStyles = makeStyles({
+
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    
+    header: {
+
+        position: "absolute",
+        backgroundColor: "transparent",
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        boxShadow: "0px 0px 0px 0px"
+    },
+
+    headerToolBar: {
+
+        width: "80%",
+        margin: "0 auto",
+    },
+
+    iconButtonProfile : {
+
+        ml: "2",
+        position: "absolute",
+        right: "-100px",
+    },
+
+    avatarIcon: {
+
+        width: "35px",
+        height: "35px",
+        backgroundColor: "#2ecc71",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+
+    },
+
+    iconButtonNotification: {
+
+        ml: "2",
+        position: "absolute",
+        right: "-50px",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s ",
+        },
+
+    },
+
+    sideDrawer: {
+
+        width: drawerWidth,
+        flexShrink: "0",
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+    },
+
+    sideDrawerBox: {
+
+        overflow: "auto",
+    },
+
+    pageLink: {
+
+        textDecoration: "none",
+        "&:focus,&:hover": {
+
+            textDecoration: "none",
+        },
+    },
+
+    drawerListItem: {
+
+        padding: "20px",
+    },
+
+    drawerListIcon: {
+
+        color: "#2ecc71",
+        "&:hover,&:focus": {
+
+            color: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+    },
+
+    drawerListIconText: {
+
+        color: "#555555",
+        textShadow: "2px 2px rgba(85,85,85,0.15)",
+        "&:hover,&:focus": {
+
+            textShadow: "5px 5px rgba(85,85,85,0.20)",
+            transition: "ease-in-out 0.25s ",
+        },
+    },
+
+    toolCard: {
+
+        display: "flex",
+        flexDirection: "column",
+        borderRadius: "20px",
+        boxShadow: "2px 2px 2px 2px rgba(0,0,0,0.025)",
+        backgroundColor: "rgba(0,0,0,0.05)",
+        alignItems: "center",
+        width: "250px",
+        height: "350px",
+    },
+
+    toolCardMedia: {
+
+        paddingTop: "10%",
+        width: "125px",
+    },
+
+    toolCardBtn: {
+
+        color: "#fff",
+        backgroundColor: "#2ecc71",
+        width: "100px",
+        textShadow: "2px 2px rgba(0,0,0,0.10)",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+            textShadow: "5px 5px rgba(0,0,0,0.25)",
+        },
+    },
+
+    profileMenuItem: {
+
+        "&:hover": {
+
+            backgroundColor: "rgba(85,85,85,0.15)",
+        },
+
+
+    },
+
+    profileMenuTopIcons: {
+
+        color: "#fff",
+        backgroundColor: "#2ecc71",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+
+
+
+    },
+
+
+    profileMenuBottomIcons: {
+
+        color: "#555555",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s ",
+        },
+
+
+    },
+
+    drugInteractionTitle: {
+
+        color: "#555555",
+        fontSize: "32px",
+        fontFamily: "Public Sans",
+
+
+    },
+
+    drugInteractionSubTitle: {
+
+        color: "#555555",
+        fontSize: "20px",
+        fontFamily: "Public Sans",
+    },
+
+    drugInteractionNameInput: {
+
+        display: "block",
+        margin: "20px auto",
+        textAlign: "center",
+        padding: "14px 10px",
+        width: "250px",
+        
+    },
+
+    rxcuiAnswer: {
+
+        fontSize: "12px",
+        fontFamily: "Public Sans",
+        color: "#555555",
+        cursor: "pointer",
+        borderRadius: "24px",
+        margin: "10px",
+        padding: "10px",
+    },
+
+    listParagraphs: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+
+
+    },
+
+    listDrugName: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s",
+            textShadow: "5px 5px 2px rgba(85,85,85,0.45)",
+        },
+
+    },
+
+    listDrugDescription: {
+
+        color: "#555555",
+        fontFamily: "Public Sans",
+    },
+
+    moleculeStructureText: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+    },
+
+});
+
+function GetRxcuiID () {
+
+    const classes = useStyles();
 
     const [rxcuiIDLoader, updateRxcuiIDLoader] = useState(true);
     const [rxcuiID, updateRxcuiID] = useState(null);
@@ -12,8 +322,34 @@ export default () => {
     const [search, updateSearch] = useState(1);
     const [show, updateShow] = useState(false);
 
-    function nameChange(enteredName) {
-        updateName(enteredName);
+    const nameChange = event => {
+        updateName(event.target.value);
+    };
+
+    async function getRxcuiIDCall() {
+
+        const getRxcuiIDCallAPI = `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${name}&search=${search}`;
+        const getRxcuiIDCallResponse = await fetch(getRxcuiIDCallAPI)
+        const getRxcuiIDCallData = await getRxcuiIDCallResponse.json();
+
+        console.log(getRxcuiIDCallData);
+
+        if (getRxcuiIDCallData.idGroup.rxnormId !== undefined) {
+
+            updateRxcuiID(getRxcuiIDCallData.idGroup.rxnormId[0]);
+            updateRxcuiIDLoader(true);
+        }
+
+        else {
+
+            console.log("No matching Rxcui Id!");
+
+            updateRxcuiID("No Match...");
+            updateRxcuiIDLoader(true);
+            updateShow(true);
+        }
+
+        return;
     }
 
     const alertMethod = () => {
@@ -33,131 +369,79 @@ export default () => {
     const onSubmit = event => {
 
         event.preventDefault();
-
         updateRxcuiIDLoader(false);
         updateShow(false);
 
-
         (async () => {
 
-            const url = `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${name}&search=${search}`;
-            const response = await fetch(url)
-            const data = await response.json();
-
-            console.log(data);
-            
-            
-            if (data.idGroup.rxnormId !== undefined) {
-
-                console.log(data.idGroup.rxnormId[0]);
-
-                updateRxcuiID(data.idGroup.rxnormId[0]);
-                updateRxcuiIDLoader(true);
-            }
-
-            else {
-
-                console.log("No matching Rxcui Id!");
-
-                updateRxcuiID("No Match...");
-                updateRxcuiIDLoader(true);
-                updateShow(true);
-            }
-
-
-            
-
+            await getRxcuiIDCall();
+            console.log("Waited!");
         })();
-
     }
-
 
     //return method
     return (
 
-    <div>
+        <StyledEngineProvider injectFirst>
+            <div sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
-            <div className="container">
-                <a href="#" className="navbar-brand">Drug<span className="text-primary">Interactor</span></a>
+                <AppBar className={classes.header}>
+                    <Toolbar className={classes.headerToolBar}>
+                        <AppBarSearch />
+                        <Box sx={{ flexGrow: 1 }} />
 
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                        <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
+                            <AccountPopover />
+                        </Stack>
+                    </Toolbar>
+                </AppBar>
 
+                
+                <DashboardSidebar />
 
-                <div className="navbar-collapse collapse" id="navbarContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/HomePage">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/getInteractionPair">Interaction Pair Tool</a>
-                        </li>
-                        <li className="nav-link">
-                            <a className="nav-link" href="/getDrugInteraction">All Interactions Tool</a>
-                        </li>
-                        <li className="nav-link">
-                            <a className="nav-link" href="/AllDrugTerms">All Drugs Tool</a>
-                        </li>
-                        <li className="nav-link">
-                            <a className="nav-link" href="getRxcuiId">Rxcui ID Tool</a>
-                        </li>
-                        <li className="nav-link">
-                            <a className="nav-link" href="/drug3dtest">Molecule View Tool</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                <Box 
+                    component="main"
+                    sx={{
+                        flexGrow: 1,
+                        width: `calc(100% - ${drawerWidth}px)`,
+                        marginLeft: 35,
+                    }}
+                >
+                    <Toolbar />
 
+                    <div className="page-section">
+                        <div className="container">
+                            <div className="card-service-large wow fadeInUp">
+                                <form onSubmit={onSubmit}>
+                                    <h1 className="rxcui-header">Find Rxcui ID</h1>
+                                    <h2 className="rxcui-subheader">Enter drug name below</h2>
 
-        <div className="page-section">
-            <div className="container">
-                <div className="card-service-large wow fadeInUp">
-                    <form onSubmit={onSubmit}>
+                                    <TextField
+                                        label="Enter Drug Name..."
+                                        value={name}
+                                        onChange={nameChange}
+                                    />
 
-                        <h1 className="rxcui-header">Find Rxcui ID</h1>
-                        <h2 className="rxcui-subheader">Enter drug name below</h2>
+                                    <Button type="submit" className={classes.toolCardBtn}>Find</Button>
 
-                        <input
-                            className="rxcui-name-input"
-                            id="name"
-                            name="name"
-                            type="text"
-                            placeholder="Enter Drug Name..."
-                            value={name}
-                            onChange={(event) => nameChange(event.target.value)}
-                        />
-
-                        <button type="submit" className="btn btn-primary">Find</button>
-
-                        <div className="rxcui-answer-div">
-                            {alertMethod()}
-                            <p className="rxcui-answer">Rxcui ID: {rxcuiIDLoader ? rxcuiID : <ReactBootStrap.Spinner animation="border" size="sm" style={{ color: "#2ecc71" }}/> }</p>
-                        </div>
-                    </form>
-
-
-                </div>
-            </div>
-        </div>
-
-
-        <footer className="page-footer bg-image">
-                <div className="container">
-                    <div className="row mb-5">
-                        <div className="col-lg-3 py-3">
-                            <h3>Drug Interactor</h3>
-                            <p>Take substances safely</p>
-                            <p>Medical Grade Information provided by DrugBank Official Research Records</p>
-                            <p>Creator: Shae Sullivan</p>
+                                    <div className="rxcui-answer-div">
+                                        {alertMethod()}
+                                        <p className="rxcui-answer">Rxcui ID: {rxcuiIDLoader ? rxcuiID : <ReactBootStrap.Spinner animation="border" size="sm" style={{ color: "#2ecc71" }}/>}</p>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </footer>
+                </Box>
+ 
+            </div>
+        </StyledEngineProvider>
+    )
 
-    </div>
+}
 
-    );
-};
+export default GetRxcuiID;
