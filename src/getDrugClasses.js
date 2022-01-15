4e2793f9f64c6f4df6d8ebc1d0b4c8c04a989dcb
Sxlly <shaesullivan.buisness1@gmail.com>
2022-01-15 18:20:16 +0800

    addition of drug classes page

---
+++ src/getDrugClasses.js
@@ -0,0 +1,322 @@
+import React, { Component, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import './getDrugInteractionCSS.css';
+import './bootstrap.css';
+import './theme.css';
+import * as ReactBootStrap from "react-bootstrap";
+
+//importing self made components
+import AppBarSearch from './components/appBarSearch';
+import AccountPopover from './components/AccountPopOver';
+import DashboardSidebar from './components/Sidebar';
+
+//Material UI Icon Imports
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
+//Material UI Imports
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { TextField } from "@mui/material";
+import { Stack, StyledEngineProvider } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Card, CardActions, CardContent, CardMedia, Grid } from '@mui/material';
+
+
+//google font imports
+import "@fontsource/advent-pro/600.css";
+import "@fontsource/public-sans/600.css";
+
+//sidebar width constant
+const drawerWidth = 240;
+
+//material ui CSS classes
+const useStyles = makeStyles({
+
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    
+    header: {
+
+        position: "absolute",
+        backgroundColor: "transparent",
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        boxShadow: "0px 0px 0px 0px"
+    },
+
+    headerToolBar: {
+
+        width: "80%",
+        margin: "0 auto",
+    },
+
+    iconButtonProfile : {
+
+        ml: "2",
+        position: "absolute",
+        right: "-100px",
+    },
+
+    avatarIcon: {
+
+        width: "35px",
+        height: "35px",
+        backgroundColor: "#2ecc71",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+
+    },
+
+    iconButtonNotification: {
+
+        ml: "2",
+        position: "absolute",
+        right: "-50px",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s ",
+        },
+
+    },
+
+    sideDrawer: {
+
+        width: drawerWidth,
+        flexShrink: "0",
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+    },
+
+    sideDrawerBox: {
+
+        overflow: "auto",
+    },
+
+    pageLink: {
+
+        textDecoration: "none",
+        "&:focus,&:hover": {
+
+            textDecoration: "none",
+        },
+    },
+
+    drawerListItem: {
+
+        padding: "20px",
+    },
+
+    drawerListIcon: {
+
+        color: "#2ecc71",
+        "&:hover,&:focus": {
+
+            color: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+    },
+
+    drawerListIconText: {
+
+        color: "#555555",
+        textShadow: "2px 2px rgba(85,85,85,0.15)",
+        "&:hover,&:focus": {
+
+            textShadow: "5px 5px rgba(85,85,85,0.20)",
+            transition: "ease-in-out 0.25s ",
+        },
+    },
+
+    toolCard: {
+
+        display: "flex",
+        flexDirection: "column",
+        borderRadius: "20px",
+        boxShadow: "2px 2px 2px 2px rgba(0,0,0,0.025)",
+        backgroundColor: "rgba(0,0,0,0.05)",
+        alignItems: "center",
+        width: "250px",
+        height: "350px",
+    },
+
+    toolCardMedia: {
+
+        paddingTop: "10%",
+        width: "125px",
+    },
+
+    toolCardBtn: {
+
+        color: "#fff",
+        backgroundColor: "#2ecc71",
+        width: "150px",
+        textShadow: "2px 2px rgba(0,0,0,0.10)",
+        marginTop: "15px",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+            textShadow: "5px 5px rgba(0,0,0,0.25)",
+        },
+    },
+
+    profileMenuItem: {
+
+        "&:hover": {
+
+            backgroundColor: "rgba(85,85,85,0.15)",
+        },
+
+
+    },
+
+    profileMenuTopIcons: {
+
+        color: "#fff",
+        backgroundColor: "#2ecc71",
+        "&:hover,&:focus": {
+
+            backgroundColor: "#555555",
+            transition: "ease-in-out 0.25s ",
+        },
+
+
+
+    },
+
+
+    profileMenuBottomIcons: {
+
+        color: "#555555",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s ",
+        },
+
+
+    },
+
+    drugInteractionTitle: {
+
+        color: "#555555",
+        fontSize: "32px",
+        fontFamily: "Public Sans",
+
+
+    },
+
+    drugInteractionSubTitle: {
+
+        color: "#555555",
+        fontSize: "20px",
+        fontFamily: "Public Sans",
+    },
+
+    drugInteractionNameInput: {
+
+        display: "block",
+        margin: "20px auto",
+        textAlign: "center",
+        padding: "14px 10px",
+        width: "250px",
+        
+    },
+
+    rxcuiAnswer: {
+
+        fontSize: "12px",
+        fontFamily: "Public Sans",
+        color: "#555555",
+        cursor: "pointer",
+        borderRadius: "24px",
+        margin: "10px",
+        padding: "10px",
+    },
+
+    listParagraphs: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+
+
+    },
+
+    listDrugName: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+        "&:hover,&:focus": {
+
+            color: "#2ecc71",
+            transition: "ease-in-out 0.25s",
+            textShadow: "5px 5px 2px rgba(85,85,85,0.45)",
+        },
+
+    },
+
+    listDrugDescription: {
+
+        color: "#555555",
+        fontFamily: "Public Sans",
+    },
+
+    moleculeStructureText: {
+
+        fontFamily: "Public Sans",
+        color: "#555555",
+    },
+
+});
+
+function AllDrugClasses () {
+
+    return (
+
+        <div></div>
+
+    )
+}
